de986385e79967a40f9a33b60fe584afb487be1f
piusatwau <atwaupius7@gmail.com>
2024-10-22 14:29:48 +0530

    showing description fully of books table

---
+++ task_4.sql
@@ -0,0 +1,5 @@
+-- Query the INFORMATION_SCHEMA to get the full description of the Books table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books';
